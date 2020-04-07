05ef3f811dca042a4d2a715370723936652a0df6
Kevin Neyer <kevinneyer@gmail.com>
2020-04-07 14:33:08 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  small_dogs = ["Chihuahua", "Shiba Inu"]
+  dog_breeds.pop(small_dogs)
+end
