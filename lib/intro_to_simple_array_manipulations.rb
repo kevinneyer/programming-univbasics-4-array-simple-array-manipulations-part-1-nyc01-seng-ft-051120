cbb29995ab8db9bfacaa97a3e0ac63551ff51831
Kevin Neyer <kevinneyer@gmail.com>
2020-04-07 14:40:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,6 +12,5 @@ end
 
 def pop_with_args(array)
   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  small_dogs = ["Chihuahua", "Shiba Inu"]
-  dog_breeds.pop(small_dogs)
+  dog_breeds.pop(2,3)
 end
